f5ea58ec40f3e8ac768c2b55ed3cfc6ec37fd08c
akiotnk00 <akiotnk00@gmail.com>
2022-10-06 20:45:12 -0300

    Aula 06/10 Mjv School Java

--- src/testes/Blackjack.java
+++ src/testes/Blackjack.java
@@ -6,44 +6,242 @@ import java.util.Scanner;
 
 public class Blackjack {
 
+	static List<Integer> jogador;
+	static List<Integer> mesa;
+
+	// Metodo que limpa e preenche novamente um baralho (Lista de Integer).
+	public static List<Integer> resetaBaralho(List<Integer> baralho) {
+
+		// Limpa o conteudo da lista.
+		baralho.clear();
+
+		Integer i = 1;
+
+		// Preenche o baralho de 1 a 13.
+		while (i <= 13) {
+			baralho.add(i);
+			i++;
+		}
+
+		return baralho;
+	}
+
+	public static List<Integer> escolheCartaJogador(List<Integer> baralho) {
+
+		Integer posicao;
+
+		// Escolhe uma posição no baralho de acordo com o tamanho do baralho;
+		posicao = (int) (Math.random() * baralho.size());
+
+		// O jogador recebe a carta.
+		jogador.add(baralho.get(posicao));
+
+		// A carta é removida do baralho.
+		baralho.remove(posicao);
+
+		// Retorna o baralho para ele ser atualizado.
+		return baralho;
+	}
+
+	public static List<Integer> escolheCartaMesa(List<Integer> baralho) {
+
+		Integer posicao;
+
+		// Escolhe uma posição no baralho de acordo com o tamanho do baralho;
+		posicao = (int) (Math.random() * baralho.size());
+
+		// O jogador recebe a carta.
+		mesa.add(baralho.get(posicao));
+
+		// A carta é removida do baralho.
+		baralho.remove(posicao);
+
+		// Retorna o baralho para ele ser atualizado.
+		return baralho;
+	}
+
+	// Quando a opcao iniciar um jogo é escolhida.
 	public static void iniciaJogo(Scanner scan) {
-		int opcao;
-		List<Integer> jogador = new ArrayList<Integer>();
-		List<Integer> mesa = new ArrayList<Integer>();
 
-		// Inicio da partida
-		jogador.add((int) (Math.random() * 13));
-		jogador.add((int) (Math.random() * 13));
+		// Inicia as listas de baralhos.
+		List<Integer> ouro = new ArrayList<Integer>();
+		List<Integer> espada = new ArrayList<Integer>();
+		List<Integer> copa = new ArrayList<Integer>();
+		List<Integer> paus = new ArrayList<Integer>();
 
-		mesa.add((int) (Math.random() * 13));
-		mesa.add((int) (Math.random() * 13));
+		// Inicia jogadores.
+		jogador = new ArrayList<Integer>();
+		mesa = new ArrayList<Integer>();
 
-		// Mostra as cartas
-		System.out.print("Cartas do Jogador: ");
-		for (Integer carta : jogador) {
-			switch(carta) {
+		// Inicia baralhos.
+		ouro = resetaBaralho(ouro);
+		espada = resetaBaralho(espada);
+		copa = resetaBaralho(copa);
+		paus = resetaBaralho(paus);
+
+		// Sorteia as 2 cartas iniciais do jogador.
+		int contador = 0;
+		while (contador < 2) {
+			// Sorteia de 0 a 3 para saber qual o naipe.
+			switch ((int) (Math.random() * 3)) {
+			// Ouro
 			case 0:
-				System.out.print("[" + "A" + "]");
+				ouro = escolheCartaJogador(ouro);
 				break;
-			case 11:
-				System.out.print("[" + "J" + "]");
+			// Espada
+			case 1:
+				espada = escolheCartaJogador(espada);
 				break;
-			case 12:
-				System.out.print("[" + "Q" + "]");
+			// Copa
+			case 2:
+				copa = escolheCartaJogador(copa);
 				break;
-			case 13:
-				System.out.print("[" + "K" + "]");
-				break;
-			
+			// Paus
 			default:
-				System.out.print("[" + carta + "]");
-			break;
+				paus = escolheCartaJogador(paus);
+				break;
+			}
+			contador++;
+		}
+
+		// Reseta o contador;
+		contador = 0;
+
+		// Sorteia as 2 cartas inicias da mesa.
+		while (contador < 2) {
+			// Sorteia de 0 a 3 para saber qual o naipe.
+			switch ((int) (Math.random() * 3)) {
+			// Ouro
+			case 0:
+				ouro = escolheCartaMesa(ouro);
+				break;
+			// Espada
+			case 1:
+				espada = escolheCartaMesa(espada);
+				break;
+			// Copa
+			case 2:
+				copa = escolheCartaMesa(copa);
+				break;
+			// Paus
+			default:
+				paus = escolheCartaMesa(paus);
+				break;
+			}
+			contador++;
+		}
+		// Inicio da partida.
+
+		exibeJogadores();
+
+		int opcao;
+
+		System.out.println("-> Escolha uma opção");
+		System.out.println("1 - Pegar outra carta");
+		System.out.println("0 - Parar");
+
+		
+		opcao = 5;
+		
+		while (opcao != 0 && calculaCartas(jogador) <= 21) {
+			opcao = scan.nextInt();
+			
+			switch (opcao) {
+			case 0:
+				while (calculaCartas(mesa) < calculaCartas(jogador) && calculaCartas(jogador) < 21) {
+					switch ((int) (Math.random() * 3)) {
+					// Ouro
+					case 0:
+						ouro = escolheCartaMesa(ouro);
+						break;
+					// Espada
+					case 1:
+						espada = escolheCartaMesa(espada);
+						break;
+					// Copa
+					case 2:
+						copa = escolheCartaMesa(copa);
+						break;
+					// Paus
+					default:
+						paus = escolheCartaMesa(paus);
+						break;
+					}
+					exibeJogadores();
+				}
+				break;
+			case 1:
+				switch ((int) (Math.random() * 3)) {
+				// Ouro
+				case 0:
+					ouro = escolheCartaJogador(ouro);
+					break;
+				// Espada
+				case 1:
+					espada = escolheCartaJogador(espada);
+					break;
+				// Copa
+				case 2:
+					copa = escolheCartaJogador(copa);
+					break;
+				// Paus
+				default:
+					paus = escolheCartaJogador(paus);
+					break;
+				}
+				exibeJogadores();			
+				break;
+			default:
+				break;
 			}
 		}
-		System.out.println("");
+		if(calculaCartas(jogador)<=21) {
+		while (calculaCartas(mesa) < calculaCartas(jogador) && calculaCartas(jogador) < 21) {
+			switch ((int) (Math.random() * 3)) {
+			// Ouro
+			case 0:
+				ouro = escolheCartaMesa(ouro);
+				break;
+			// Espada
+			case 1:
+				espada = escolheCartaMesa(espada);
+				break;
+			// Copa
+			case 2:
+				copa = escolheCartaMesa(copa);
+				break;
+			// Paus
+			default:
+				paus = escolheCartaMesa(paus);
+				break;
+			}
+			exibeJogadores();
+		}
+		if(calculaCartas(mesa)>calculaCartas(jogador) && calculaCartas(mesa)<=21) {
+			System.out.println("Você perdeu!");
+		}
+		else
+		{
+			System.out.println("Você ganhou!");
+		}
+		}
+		else
+		{
+			System.out.println("Você perdeu!");
+		}
+	}
+
+	public static void exibeJogadores() {
+		System.out.print("Cartas do Jogador: ");
+		exibeCartasJogador(jogador);
+
 		System.out.print("Cartas da Mesa: ");
-		for (Integer cartamesa : mesa) {
-			switch(cartamesa) {
+		exibeCartasJogador(mesa);
+	}
+
+	public static void exibeCartasJogador(List<Integer> cartasjogador) {
+		for (Integer carta : cartasjogador) {
+			switch (carta) {
 			case 0:
 				System.out.print("[" + "A" + "]");
 				break;
@@ -57,12 +255,19 @@ public class Blackjack {
 				System.out.print("[" + "K" + "]");
 				break;
 			default:
-				System.out.print("[" + cartamesa + "]");
-			break;
+				System.out.print("[" + carta + "]");
+				break;
+			}
 		}
-		}
+		System.out.print(" # Total: " + calculaCartas(cartasjogador));
 		System.out.println("");
-		
+	}
+
+	public static double calculaCartas(List<Integer> cartasjogador) {
+
+		Integer soma = cartasjogador.stream().mapToInt(i -> i.intValue() > 10 ? 10 : i.intValue()).sum();
+
+		return soma;
 	}
 
 	// Classe Main
@@ -80,7 +285,6 @@ public class Blackjack {
 		if (opcao == 1) {
 			iniciaJogo(scan);
 		}
-
 	}
 
 }
---
+++ src/testes/DinamicaException.java
@@ -0,0 +1,66 @@
+package testes;
+
+public class DinamicaException {
+
+	public static int resultadoEntreDois(Integer numero1, Integer numero2) throws Exception {
+
+		// Se o primeiro parametro for menor que 0
+		if (numero1 < 0) {
+			
+			// Se o segundo for maior que 10.
+			if (numero2 > 10) {
+				throw new Exception(
+						"O primeiro parametro não pode ser menor que 0 e o segundo parametro não pode ser maior que 10.");
+			} else {
+				throw new Exception("O primeiro parametro não pode ser menor que 0.");
+			}
+			
+			// Primeiro maior ou igual a 0 e o segundo for maior que 10.
+		} else if (numero2 > 10) {
+			throw new Exception("O segundo parametro não pode ser maior que 10.");
+		} else {
+			return Math.max(numero1, numero2) - Math.min(numero1, numero2);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// Teste com o primeiro menor que 0.
+		try {
+			resultadoEntreDois(-1, 5);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Teste com o segundo maior que 10.
+		try {
+			resultadoEntreDois(3, 15);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Teste com o primeiro menor que 0 e o segundo maior que 10.
+		try {
+			resultadoEntreDois(-4, 12);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Teste com o segundo numero maior que o primeiro.
+		try {
+			System.out.println(resultadoEntreDois(2, 8));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// Teste com o primeiro numero maior que o segundo.
+		try {
+			System.out.println(resultadoEntreDois(5, 3));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
